3dc14874c027228410d3a74b762aef9013cdc1d4
peanut-lover <jihoson94@gmail.com>
2022-05-28 22:05:21 +0900

    :wrench: Setting NextJS + tailwindCSS
    using public documents

---
+++ pages/card.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Card: React.FC = () => {
+    
+    return <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+
+    </div>
+} 
