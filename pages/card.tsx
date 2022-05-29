0059feeb1212795141a75a6d3e4cedafac4145eb
peanut-lover <jihoson94@gmail.com>
2022-05-30 00:29:36 +0900

    :wrench: Change project name to one-tool

--- pages/card.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Card: React.FC = () => {
-    return (
-        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"></div>
-    );
-};
