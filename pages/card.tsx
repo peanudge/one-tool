869c71c5c8c535b35772121181faabb3712f386f
peanut-lover <jihoson94@gmail.com>
2022-05-29 12:40:31 +0900

    :wrench: Add eslintrc.json

--- pages/card.tsx
+++ pages/card.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 export const Card: React.FC = () => {
-    
-    return <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-
-    </div>
-} 
+    return (
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"></div>
+    );
+};
